a2bfd65ad3edace3b7198a678e265e64e6a9474f
bingodfok <bingodfok@gmail.com>
2024-09-22 22:20:38 +0800

    Spring 环境

---
+++ responsibility-chain/src/main/java/com/cobin/dp/ChainApplication.java
@@ -0,0 +1,19 @@
+package com.cobin.dp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ChainApplication
+ *
+ * @author bingodfok
+ * @date 2024/9/21 下午10:10
+ **/
+@SpringBootApplication
+public class ChainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChainApplication.class).close();
+    }
+
+}
